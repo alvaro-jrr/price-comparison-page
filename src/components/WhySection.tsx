868c163bd497bd047192ef57835dbadc60962c4e
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-02-24 15:06:52 -0400

    add content to why section

--- src/components/WhySection.tsx
+++ src/components/WhySection.tsx
@@ -1,7 +1,29 @@
 export default function WhySection() {
     return (
         <section>
-            <h3>¿Por qué ShopEasy?</h3>
+            <h3 className="text-gray-900 text-3xl font-bold sm:text-4xl">
+                ¿Por qué ShopWisely?
+            </h3>
+
+            <p className="text-slate-600">
+                Sabemos que las personas desean ahorrar más dinero al realizar
+                sus compras y <em>evitar</em> largas. Se hace muy costoso y se
+                pierde mucho tiempo ir a cada supermercado y tienda de la ciudad
+                para saber en donde están los mejores precios.
+            </p>
+
+            <p className="text-slate-600">
+                Así es como nace ShopWisely, una aplicación que te conecta con
+                las diversas tiendas y te permite comparar los precios de un
+                mismo producto y así elegir el mejor, pero no nos conformamos
+                con eso.
+            </p>
+
+            <p className="text-slate-600">
+                Para brindarte la <em>mejor</em> experiencia, ofrecemos un
+                servicio de entrega rápido que hace llegar los productos que
+                pediste en cuestión de minutos.
+            </p>
         </section>
     );
 }
