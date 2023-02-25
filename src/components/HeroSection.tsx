31236600062f7c3348123d6c63432ace9cff13c5
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-02-25 11:22:53 -0400

    set navbar as sticky

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,16 +14,16 @@ export default function HeroSection() {
 				</Paragraph>
 			</div>
 
-			<div className="flex gap-x-4 md:justify-center">
+			<div className="flex flex-col gap-y-2 gap-x-4 md:flex-row md:justify-center">
 				<a
-					className="block rounded-full px-6 py-3 bg-blue-500 font-medium text-white transition-colors hover:bg-blue-600"
+					className="w-fit block rounded-full px-6 py-3 bg-blue-500 font-medium text-white transition-colors hover:bg-blue-600"
 					href="#why"
 				>
 					Conocer más
 				</a>
 
 				<a
-					className="block rounded-full px-6 py-3 border font-medium border-blue-200 text-blue-500 hover:bg-blue-50"
+					className="block w-fit rounded-full px-6 py-3 border font-medium border-blue-200 text-blue-500 hover:bg-blue-50"
 					href="#feature"
 				>
 					Características
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,7 +15,7 @@ function MobileMenu() {
 			<button
 				aria-controls="mobile-navigation"
 				aria-expanded={isOpen}
-				className="relative z-10 p-2 text-slate-900"
+				className="relative z-10 p-2 text-gray-900"
 				onClick={() => setIsOpen((isOpen) => !isOpen)}
 			>
 				{isOpen ? (
@@ -49,12 +49,12 @@ function MobileMenu() {
 
 export default function NavBar() {
 	return (
-		<header className="px-[5vw] relative top-0 bg-white z-10 py-8">
-			<div className="flex justify-between">
+		<header className="px-[5vw] sticky top-0 bg-white z-10 py-6 md:py-8">
+			<div className="flex items-center justify-between">
 				<img
 					alt="Logo de ShopWisely"
 					src="/assets/logo.svg"
-					className="h-10"
+					className="h-8 md:h-10"
 				/>
 
 				<nav>
@@ -63,7 +63,7 @@ export default function NavBar() {
 							<li key={link.to}>
 								<a
 									href={link.to}
-									className="block px-5 py-2 font-medium text-slate-500 transition-colors hover:bg-gray-100 rounded-full"
+									className="block px-5 py-2 font-medium text-gray-500 transition-colors hover:bg-gray-100 rounded-full"
 								>
 									{link.name}
 								</a>
